87d8377d827077ea1fe77d6a1a8ff1c863d04595
Ekaterina Chernikova <5492432-pepe_love@users.noreply.gitlab.com>
2024-05-28 19:10:18 +0700

    Update Slot.cs
    -added timer for limited item

--- Assets/Scripts/Slot.cs
+++ Assets/Scripts/Slot.cs
@@ -2,15 +2,20 @@ using System;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using System.Collections;
+using System.Net;
+using System.Globalization;
 
 public class Slot : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI title, content;
     [SerializeField] private Button[] purchaseButtons;
-
+    private WaitForSecondsRealtime delay = new WaitForSecondsRealtime(1);
+    private Coroutine timer = null;
     public void Initialize(Item item)
     {
         title.text = item.Name;
+        if (item is TimeLimitedItem) SetTimer(((TimeLimitedItem)item).Limit);
         int index = 0;
         foreach(Currency currency in System.Enum.GetValues(typeof(Currency))) 
         {
@@ -21,15 +26,50 @@ public class Slot : MonoBehaviour
             }
         }
     }
+    private DateTime GetCurrentDateTime()
+    {
+        try
+        {
+            using (var response = WebRequest.Create("http://www.google.com").GetResponse())
+                return DateTime.ParseExact(response.Headers["date"],
+                    "ddd, dd MMM yyyy HH:mm:ss 'GMT'",
+                    CultureInfo.InvariantCulture.DateTimeFormat,
+                    DateTimeStyles.AssumeUniversal);
+        }
+        catch (WebException)
+        {
+            return DateTime.Now;
+        }
+    }
+
+    private void SetTimer(DateTime limit)
+    {
+        timer = StartCoroutine(UpdateTimer(limit));
+    }
+
+    private IEnumerator UpdateTimer(DateTime limit)
+    {
+        TimeSpan timeRemaining;
+        do {
+            timeRemaining = limit - GetCurrentDateTime();
+            content.text = $"{timeRemaining.Hours:D2} : {timeRemaining.Minutes:D2} : {timeRemaining.Seconds:D2}";
+            yield return delay;
+        } while (timeRemaining > TimeSpan.Zero);
+        content.text = "Expired";
+        LockItem();
+    }
 
     private void PurchaseItem(IPurchaseMethod purchaseMethod, Item item) 
     {
         item.Purchase(purchaseMethod);
-        content.text = "purchased";
-        foreach (Button button in purchaseButtons)
-        {
-            button.interactable = false;
-        }
+        if (timer != null) StopCoroutine(timer);
+        content.text = "Purchased";
+        LockItem();
+    }
+
+    private void LockItem()
+    {
+        foreach (Button button in purchaseButtons) button.interactable = false;
     }
 
     IPurchaseMethod GetMethod(Currency currency) 
@@ -41,5 +81,5 @@ public class Slot : MonoBehaviour
             _ => throw new ArgumentOutOfRangeException(nameof(currency), currency, null)
         };
     }
-    
+
 }
